5452600d95903ea08f4284ed5ee7789b83aa8b6d
Swas.py <codewithswastik@gmail.com>
2024-10-26 22:56:08 +0530

    feat: Add HomePage

---
+++ src/hoodplanner/ui/HomePage.java
@@ -0,0 +1,12 @@
+package hoodplanner.ui;
+
+import javax.swing.JFrame;
+
+public class HomePage extends JFrame {
+    public HomePage() {
+        setTitle("Hood Planner");
+        setSize(800, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+}
